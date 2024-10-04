a269d34804f95ebc9279501bca629b475b4ad707
Waseem Azmy <waseemazmy21@gmail.com>
2024-10-04 21:00:36 +0300

    Add Custom function to handle express validator resutls

---
+++ src/middlewares/validate.js
@@ -0,0 +1,16 @@
+import { validationResult } from "express-validator";
+
+export function handleValidationErrors(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorsMessages = errors.array().reduce((acc, error) => {
+      console.error(error);
+      if (!acc.hasOwnProperty(error.path)) {
+        acc[error.path] = error.msg;
+      }
+      return acc;
+    }, {});
+    return res.status(400).json({ errors: errorsMessages });
+  }
+  next();
+}
